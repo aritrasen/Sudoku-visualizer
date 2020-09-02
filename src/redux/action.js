1fad28890319acac0116c66b251b3d7776d77f1d
aritrasen <aritrasen15051990@gmail.com>
2020-09-01 20:49:34 -0500

    Modified solved button

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,7 +1,19 @@
-const createGrid = ()=> ({type: "CREATE_GRID"})
-const changeGrid = (props) => ({type: "CHANGE_GRID", payload:{row: props.row,col: props.col,val: props.val}})
-const solveGrid = (props) => ({type: "BACKTRACK", payload:{row: props.row,col: props.col,val: props.val}})
+const createGrid = () => ({ type: "CREATE_GRID" });
+const changeGrid = (props) => ({
+  type: "CHANGE_GRID",
+  payload: { row: props.row, col: props.col, val: props.val },
+});
+const solveGrid = (props) => ({
+  type: "BACKTRACK",
+  payload: { row: props.row, col: props.col, val: props.val },
+});
+
+const createBacktrackActionListCount = (props) => ({
+  type: "CREATE_ACTION_LIST_COUNT",
+  payload: { listCount: props },
+});
 
 export default createGrid;
-export {changeGrid};
-export {solveGrid};
+export { changeGrid };
+export { solveGrid };
+export { createBacktrackActionListCount, createGrid };
